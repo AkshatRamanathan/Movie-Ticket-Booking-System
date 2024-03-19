34b961769cf7039916dc8b7b075af62d6d6a2ec7
AkshatRamanathan <ramanathan.ak@northeastern.edu>
2024-03-19 00:42:13 -0400

    mac porting

--- src/main/java/midterm/listner/MongoListner.java
+++ src/main/java/midterm/listner/MongoListner.java
@@ -15,33 +15,33 @@ import com.mongodb.MongoException;
 @WebListener
 public class MongoListner implements ServletContextListener {
 
-    /**
-     * Default constructor. 
-     */
-    public MongoListner() {
-        // TODO Auto-generated constructor stub
-    }
+	/**
+	 * Default constructor.
+	 */
+	public MongoListner() {
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
-     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
-     */
-    public void contextDestroyed(ServletContextEvent sce)  { 
-         // TODO Auto-generated method stub
-    }
+	 * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+	 */
+	public void contextDestroyed(ServletContextEvent sce) {
+		// TODO Auto-generated method stub
+	}
 
 	/**
-     * @see ServletContextListener#contextInitialized(ServletContextEvent)
-     */
-    public void contextInitialized(ServletContextEvent sce)  { 
-         // TODO Auto-generated method stub
-    	try {
+	 * @see ServletContextListener#contextInitialized(ServletContextEvent)
+	 */
+	public void contextInitialized(ServletContextEvent sce) {
+		// TODO Auto-generated method stub
+		try {
 			ServletContext application = sce.getServletContext();
 			MongoClient mongoDBClient = new MongoClient(application.getInitParameter("MONGODB_HOST"),Integer.parseInt(application.getInitParameter("MONGODB_PORT")));
-			System.out.println("MongoDBClient initialized successfully");
+					System.out.println("MongoDBClient initialized successfully");
 			application.setAttribute("mongodbClient", mongoDBClient);
 		} catch (MongoException e) {
 			throw new RuntimeException("MongoClient init failed");
 		}
-    }
-	
+	}
+
 }
